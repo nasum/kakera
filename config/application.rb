880445fc0ac42c0b14dbff7595d442a0ba13955c
Masaya Nasu <tomato.wonder.life@gmail.com>
2016-02-26 23:59:56 +0900

    修正

--- config/application.rb
+++ config/application.rb
@@ -28,8 +28,8 @@ module Kakera
     config.api_only = true
 
     config.action_dispatch.default_headers.merge!({
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Request-Method': '*'
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => '*'
     })
   end
 end
